b8a3a9558e035b0c26aef05a5199186f53ac759b
Beothorn <lucass@gmail.com>
2024-06-27 20:29:55 +0200

    Tray with notification

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,41 @@
-package org.example
+import java.awt.*
+import java.awt.event.ActionEvent
+import javax.swing.ImageIcon
 
 fun main() {
-    println("Hello World!")
+    if (SystemTray.isSupported()) {
+        createTrayIcon()
+    } else {
+        println("System tray is not supported on this platform.")
+    }
+}
+
+fun createTrayIcon() {
+    try {
+        val tray = SystemTray.getSystemTray()
+
+        // Create an image icon
+        val iconImage = ImageIcon("path_to_your_icon.png").image
+
+        // Create popup menu
+        val popup = PopupMenu()
+
+        // Create a menu item
+        val exitItem = MenuItem("Exit")
+        exitItem.addActionListener { _: ActionEvent? ->
+            // Handle exit action
+            System.exit(0)
+        }
+        popup.add(exitItem)
+
+        // Create a tray icon
+        val trayIcon = TrayIcon(iconImage, "Tray Icon Example", popup)
+        trayIcon.isImageAutoSize = true
+
+        // Add the tray icon to the system tray
+        tray.add(trayIcon)
+        trayIcon.displayMessage("Hello, World", "Tray Icon Example Started", TrayIcon.MessageType.INFO)
+    } catch (e: AWTException) {
+        e.printStackTrace()
+    }
 }
